247b5c941eb63959081b731d6c280449dbebd710
Scott Carter <6565704+scottcarterco@users.noreply.github.com>
2023-03-31 14:51:39 -0700

    Update code.ts
    Add color functions

--- code.ts
+++ code.ts
@@ -30,3 +30,37 @@ function generateHarmonyColors(color: string, harmonyType: string): string[] {
 function rgbToHex(r: number, g: number, b: number): string {
   return '#' + [r, g, b].map(x => x.toString(16).padStart(2, '0')).join('');
 }
+
+function generateHarmonyColors(color: string, harmonyType: string): string[] {
+  const hslColor = hexToHsl(color);
+  let harmonyColors = [];
+
+  switch (harmonyType) {
+    case 'complementary':
+      harmonyColors = [rotateHue(hslColor, 180)];
+      break;
+    case 'analogous':
+      harmonyColors = [rotateHue(hslColor, -30), rotateHue(hslColor, 30)];
+      break;
+    case 'triadic':
+      harmonyColors = [rotateHue(hslColor, 120), rotateHue(hslColor, 240)];
+      break;
+  }
+
+  return harmonyColors.map((hsl) => hslToHex(hsl));
+}
+
+function hexToHsl(hex: string): [number, number, number] {
+  const rgb = hexToRgb(hex);
+  return rgbToHsl(rgb[0], rgb[1], rgb[2]);
+}
+
+function hslToHex(hsl: [number, number, number]): string {
+  const rgb = hslToRgb(hsl[0], hsl[1], hsl[2]);
+  return rgbToHex(rgb[0], rgb[1], rgb[2]);
+}
+
+function rotateHue(hsl: [number, number, number], degrees: number): [number, number, number] {
+  return [(hsl[0] + degrees + 360) % 360, hsl[1], hsl[2]];
+}
+
